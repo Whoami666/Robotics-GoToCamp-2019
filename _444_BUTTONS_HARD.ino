63e8c02042b9f238f3971274c3823a5ef816dea3
Марина Тюльпакова <marinalovescake11@gmail.com>
2019-06-18 09:52:29 +0300

    Add files via upload

---
+++ _444_BUTTONS_HARD.ino
@@ -0,0 +1,47 @@
+ #define LED1 3
+ #define LED2 11
+ #define POT A0
+ #define BUT 2
+ int buttonState = 0;
+ int prevbuttonState = 0;   
+ int reserve = 0;
+ boolean state = 0;
+void setup() 
+{
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(BUT, INPUT);
+  pinMode(POT, INPUT);
+  Serial.begin(9600);
+ 
+  pinMode(BUT, INPUT);
+}
+void loop() 
+{  
+  int current = analogRead(POT);
+  buttonState = digitalRead(BUT);  
+  current = current / 4; 
+
+  if((prevbuttonState == LOW) && (buttonState == HIGH))
+  {
+    state = !state;
+    prevbuttonState = LOW;
+    delay(100);
+  }
+  if(state == 0)
+            {
+                  analogWrite(LED1,current);
+            }
+         else
+            {
+                  analogWrite(LED2,current); 
+            }
+     Serial.println(buttonState);
+     delay(100);
+  }
+  
+
+ // analogWrite(led1,map(analogRead(pot),0,1023,255,0)); 
+  //analogWrite(led2,map(analogRead(pot),0,1023,0,255)); 
+
+
---
+++ the_1st_program_with_lightstuff.ino
@@ -0,0 +1,17 @@
+ #define led1 3
+ #define led2 5
+ #define pot A0
+void setup() 
+{
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(pot, INPUT);
+}
+void loop() 
+{
+  int p;  
+  p = analogRead(pot);  
+  analogWrite(led1,map(analogRead(pot),0,1023,255,0)); 
+  analogWrite(led2,map(analogRead(pot),0,1023,0,255)); 
+}
+
